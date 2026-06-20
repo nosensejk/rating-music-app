d7cd4d4205656433863e6bce0546d4332a071a2c
nosensejk <antoxatur@gmail.com>
2026-06-20 17:16:48 +0300

    markup changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import Home from './pages/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AlbumPage from './pages/AlbumPage'
-import Profile from './pages/Profile'
-import Header from './components/Header'
-import Search from './pages/Search'
-import ArtistPage from './pages/ArtistPage'
-import Settings from './pages/Settings'
-import UserProfile from './pages/UserProfile'
-import { Footer } from './components/Footer'
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AlbumPage from "./pages/AlbumPage";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Search from "./pages/Search";
+import ArtistPage from "./pages/ArtistPage";
+import Settings from "./pages/Settings";
+import UserProfile from "./pages/UserProfile";
+import { Footer } from "./components/Footer";
 
 function App() {
-
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/album/:id' element={<AlbumPage/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='/artist/:id' element={<ArtistPage/>}/>
-        <Route path='/settings' element={<Settings/>}/>
-        <Route path='/u/:username' element={<UserProfile/>}/>
-      </Routes>
-      <Footer/>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/album/:id" element={<AlbumPage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/artist/:id" element={<ArtistPage />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/u/:username" element={<UserProfile />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
